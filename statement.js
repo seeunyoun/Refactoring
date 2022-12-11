09ebd474aa140e4b93ec749fd126e66daf7ad330
Youn Se Eun <tpdms717@gmail.com>
2022-12-11 22:08:45 +0900

    usd 함수 추출하기

--- statement.js
+++ statement.js
@@ -2,22 +2,17 @@ export function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
   let result = `청구내역 (고객명: ${invoice.customer})\n`
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 2,
-  }).format
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${
       perf.audience
     }석\n`
     totalAmount += amountFor(perf)
   }
-  result += `총액 ${format(totalAmount / 100)}\n`
+  result += `총액 ${usd(totalAmount)}\n`
   result += `적립 포인트 ${volumeCredits}점\n`
 
   return result
@@ -60,4 +55,12 @@ function volumeCreditsFor(aPerformance) {
     result += Math.floor(aPerformance.audience / 5)
   }
   return result
+}
+
+function usd(aNumber) {
+  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+  }).format(aNumber / 100)
 }
