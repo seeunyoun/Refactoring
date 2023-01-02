e2c56335afa55aa5fa84ac0b009e64c475c06732
Youn Se Eun <tpdms717@gmail.com>
2023-01-02 21:14:43 +0900

    Extract printDetails and printBanner function

--- 1/1.js
+++ 1/1.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
   let outstanding = 0
 
-  console.log('***********')
-  console.log('**고객 채무**')
-  console.log('***********')
+  printBanner()
 
   for (const o of invoice.orders) {
     outstanding += o.amount
@@ -16,6 +14,16 @@ function printOwing(invoice) {
     today.getDate() + 30,
   )
 
+  printDetails()
+}
+
+function printBanner() {
+  console.log('***********')
+  console.log('**고객 채무**')
+  console.log('***********')
+}
+
+function printDetails() {
   console.log(`고객명: ${invoice.customer}`)
   console.log(`채무액: ${outstanding}`)
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
