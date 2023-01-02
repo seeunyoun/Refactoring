21885806e44963d5e2f4c52d56558307b641f619
Youn Se Eun <tpdms717@gmail.com>
2023-01-02 22:02:50 +0900

    Add code

--- 5/5.js
+++ 5/5.js
@@ -1,3 +1,11 @@
 function circumference(radius) {
   return 2 * Math.PI * radius
 }
+
+class Book {
+  constructor() {}
+
+  addReservation(customer) {
+    this._reservations.push(customer)
+  }
+}
