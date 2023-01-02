963e1f2721a69442f5893397c2b95d716a4152d4
Youn Se Eun <tpdms717@gmail.com>
2023-01-02 23:06:56 +0900

    Refactor code

--- 9/9.js
+++ 9/9.js
@@ -1,21 +1,55 @@
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 }
 
 const client1 = () => {
-  const aReading = acquireReading()
-  const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity
+  const rawReading = acquireReading()
+  const aReading = new Reading(rawReading)
+  const baseCharge = aReading.baseCharge
 }
 
 const client2 = () => {
-  const aReading = acquireReading()
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity
-  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year))
+  const rawReading = acquireReading()
+  const aReading = new Reading(rawReading)
+  const taxableCharge = Math.max(
+    0,
+    aReading.baseCharge - taxThreshold(aReading.year),
+  )
 }
 
 const client3 = () => {
-  const aReading = acquireReading()
-  const basicChargeAmount = calculateBaseChage(aReading)
+  const rawReading = acquireReading()
+  const aReading = new Reading(rawReading)
+  const taxableCharge = aReading.taxableCharge
+}
 
-  function calculateBaseChage(aReading) {
-    return baseRate(aReading.month, aReading.year) * aReading.quantity
+class Reading {
+  constructor(data) {
+    this._customer = data.customer
+    this._quantity = data.quantity
+    this._month = data.month
+    this._year = data.year
+  }
+
+  get customer() {
+    return this._customer
+  }
+
+  get quantity() {
+    return this._quantity
+  }
+
+  get month() {
+    return this._month
+  }
+
+  get year() {
+    return this._year
+  }
+
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity
+  }
+
+  get taxableCharge() {
+    return Math.max(0, this.baseCharge - taxThreshold(this.year))
   }
 }
