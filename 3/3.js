7d4d009d4e6d36edd13ae6f7d5c67f1ccf99658f
Youn Se Eun <tpdms717@gmail.com>
2023-01-02 21:50:37 +0900

    Refactor code with polymorphism OOP Class

--- 3/3.js
+++ 3/3.js
@@ -6,3 +6,33 @@ function price(order) {
 
   return basePrice - quantityDiscount + shipping
 }
+
+class Order {
+  constructor() {
+    this._data = aRecord
+  }
+
+  get quantity() {
+    return this._data.quantity
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice
+  }
+
+  get price() {
+    return this.basePrice - this.quantityDiscount + this.shipping
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05
+  }
+
+  get shipping() {
+    Math.min(this.quantity * this.itemPrice * 0.1, 100)
+  }
+}
