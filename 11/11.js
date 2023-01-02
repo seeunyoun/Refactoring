cd5ca7eeb54f02ec35405f35b6a9637de7a853c0
Youn Se Eun <tpdms717@gmail.com>
2023-01-02 23:38:58 +0900

    Refactor code

--- 11/11.js
+++ 11/11.js
@@ -1,16 +1,6 @@
 function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate
-  const priceData = {
-    basePrice: basePrice,
-    quantity: quantity,
-    discount: discount,
-  }
-  const price = applyShipping(priceData, shippingMethod, discount)
-  return price
+  const priceData = calculatePricingData(product, quantity)
+  return applyShipping(priceData, shippingMethod, discount)
 }
 
 function applyShipping(priceData, shippingMethod) {
@@ -19,6 +9,19 @@ function applyShipping(priceData, shippingMethod) {
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase
   const shippingCost = priceData.quantity * shippingPerCase
-  const price = priceData.basePrice - priceData.discount + shippingCost
-  return price
+  return priceData.basePrice - priceData.discount + shippingCost
+}
+
+function calculatePricingData(data, quantity) {
+  const basePrice = product.basePrice * quantity
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate
+
+  return {
+    basePrice,
+    quantity,
+    discount,
+  }
 }
