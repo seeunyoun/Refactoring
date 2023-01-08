08f575d7f2ce0c073919797d01f3ddbdb6c214ae
Youn Se Eun <tpdms717@gmail.com>
2023-01-08 13:27:03 +0900

    Refactor 6

---
+++ CHATPER7/7-4/2.js
@@ -0,0 +1,23 @@
+class Order {
+  constructor(quantity, item) {
+    this._quantity = quantity
+    this._item = item
+  }
+
+  get price() {
+    // basePrice와 discountFactor를 읽기 전용 변수로 바꾼다.
+    // 변수를 인라인 한다.
+    // const basePrice = this.basePrice
+    const discountFactor = 0.98
+
+    if (this.basePrice > 1000) {
+      return (discountFactor -= 0.03)
+    }
+    return this.basePrice * discountFactor
+  }
+
+  // 대입문의 우변을 게터로 추출한다.
+  get basePrice() {
+    return this._quantity * this._item.price
+  }
+}
