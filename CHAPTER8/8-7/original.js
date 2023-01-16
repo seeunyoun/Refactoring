1cdc99711978ec21284a00fd6c53a7ad367f4b6b
Youn Se Eun <tpdms717@gmail.com>
2023-01-16 21:35:19 +0900

    Add original code

---
+++ CHAPTER8/8-7/original.js
@@ -0,0 +1,10 @@
+let youngest = people[0] ? people[0].age : Infinity
+let totalSalary = 0
+for (const p of people) {
+  if (p.age < youngest) {
+    return (youngest = p.age)
+  }
+  totalSalary += p.salary
+}
+
+return `최연소: ${youngest}, 총 급여: ${totalSalary}`
