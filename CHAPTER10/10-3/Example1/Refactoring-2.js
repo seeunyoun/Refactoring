6ce32c6d2adbfb2466780dd96c13155006b78215
Youn Se Eun <tpdms717@gmail.com>
2023-02-02 22:30:09 +0900

    Refactor code step 4

---
+++ CHAPTER10/10-3/Example1/Refactoring-2.js
@@ -0,0 +1,17 @@
+function payAmount(employee) {
+  // 4. result 변수는 필요 없으므로 지운다.
+  // let result
+  // 1. 최상위 조건부터 보호 구문으로 바꾼다. -> 2. 테스트하고 -> 3. 반복한다.
+  if (employee.isSeparated) {
+    return { amount: 0, reasonCode: 'SEP' }
+  }
+  if (employee.isRetired) {
+    return { amount: 0, reasonCode: 'RET' }
+  }
+  // 급여 계산 로직
+  lorem.ipsum(dolor.sitAmet)
+  consectetur(adipiscing).elit()
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua)
+  ut.enim.ad(minim.veniam)
+  return someFinalComputation()
+}
