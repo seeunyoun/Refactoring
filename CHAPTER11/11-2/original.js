66df9c6312fff531e85aa8c8612282e23170402b
Youn Se Eun <tpdms717@gmail.com>
2023-02-11 23:11:24 +0900

    Refactor code step 1~3

--- CHAPTER11/11-2/original.js
+++ CHAPTER11/11-2/original.js
@@ -18,3 +18,32 @@ function middleBand(usage) {
 function topBand(usage) {
   return usage > 200 ? usage - 200 : 0
 }
+
+// 1. 범위를 다루는 로직에서는 대개 중간에 해당하는 함수에서 시작하는 게 좋다. -> 함수 바꾸기를 적용한다.
+// middleBand -> withinBand
+// withinBand 매개변수를 추가하고 다른 호출들을 여기에 맞춘다.
+function withinBand(usage, bottom, top) {
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0
+}
+
+function baseCharge(usage) {
+  if (usage < 0) {
+    return usd(0)
+  }
+  // 2. middleBand -> withinBand
+  const amount =
+    bottomBand(usage) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    topBand(usage) * 0.07
+  return usd(amount)
+}
+
+function bottomBand(usage) {
+  return Math.min(usage, 100)
+}
+
+function topBand(usage) {
+  return usage > 200 ? usage - 200 : 0
+}
+
+// 3. 테스트한다.
