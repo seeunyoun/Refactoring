87bdd74c4534d0ad7d7d010e48e1aca010274ae8
Youn Se Eun <tpdms717@gmail.com>
2023-02-11 22:31:46 +0900

    Refactor code step 4~5

--- CHAPTER11/11-1/original.js
+++ CHAPTER11/11-1/original.js
@@ -30,15 +30,20 @@ function findMiscreant(people) {
 
 // 3. 정적 검사를 수행한다.
 
+// 4. 원래 함수를 호출하는 곳을 찾아서 새로운 질의 함수를 호출하도록 바꾸고
+// 이어서 변경 함수를 호출하는 코드를 아래에 삽입한다.
+const found = alertForMiscreant(people)
+const found = findMiscreant(people)
+alertForMiscreant()
+
+// 5. 원래 변경 함수에서 질의 관련 코드를 없앤다.
 function alertForMiscreant(people) {
   for (const p of people) {
     if (p === '조커') {
-      setOffAlarms()
-      return '조커'
+      return setOffAlarms()
     }
     if (p === '사루만') {
-      setOffAlarms()
-      return '사루만'
+      return setOffAlarms()
     }
   }
   return ''
