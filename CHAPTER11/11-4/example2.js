6ff5204eb34ec492d9823ff7f6a94634f3c49968
Youn Se Eun <tpdms717@gmail.com>
2023-02-13 00:14:36 +0900

    Refactor code step 1~3

--- CHAPTER11/11-4/example2.js
+++ CHAPTER11/11-4/example2.js
@@ -4,3 +4,36 @@ const high = aRoom.daysTempRange.high
 if (!aPlan.withinRange(low, high)) {
   alert.push('방 온도가 지정 범위를 벗어났습니다.')
 }
+
+// ----------------------------------------------------------------------------------------
+
+// 호출자
+// 2. 입력 매개변수를 추출한다.
+const tempRange = aRoom.daysTempRange
+const low = tempRange.low
+const high = tempRange.high
+// 1. '변수 추출하기' 리팩터링을 한다.
+const isWithinRange = aPlan.withinRange(low, high)
+if (!isWithinRange) {
+  alert.push('방 온도가 지정 범위를 벗어났습니다.')
+}
+
+// ----------------------------------------------------------------------------------------
+
+// 호출자
+// 2. 입력 매개변수를 추출한다.
+const tempRange = aRoom.daysTempRange
+
+// 1. '변수 추출하기' 리팩터링을 한다.
+const isWithinRange = xxNEWwithinRange(aPlan, tempRange)
+if (!isWithinRange) {
+  alert.push('방 온도가 지정 범위를 벗어났습니다.')
+}
+
+// 3. '함수 추출하기'로 새 매서드를 만든다.
+function xxNEWwithinRange(aPlan, tempRange) {
+  const low = tempRange.low
+  const high = tempRange.high
+  const isWithinRange = aPlan.withinRange(low, high)
+  return isWithinRange
+}
