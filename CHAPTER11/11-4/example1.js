724ce709f65f436b139cb30673c1033df58844c8
Youn Se Eun <tpdms717@gmail.com>
2023-02-12 23:52:43 +0900

    Add original code

---
+++ CHAPTER11/11-4/example1.js
@@ -0,0 +1,14 @@
+// 호출자
+const low = aRoom.daysTempRange.low
+const high = aRoom.daysTempRange.high
+if (!aPlan.withinRange(low, high)) {
+  alert.push('방 온도가 지정 범위를 벗어났습니다.')
+}
+
+class HeatingPlan {
+  withinRange(bottom, top) {
+    return (
+      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+    )
+  }
+}
