a8b475be2a0617c2e0c8e71304942e94ab4a4879
Youn Se Eun <tpdms717@gmail.com>
2023-02-12 11:39:29 +0900

    Refactor code

--- CHAPTER11/11-3/original.js
+++ CHAPTER11/11-3/original.js
@@ -55,3 +55,43 @@ function regularDeliveryDate(anOrder) {
 // 함수를 호출하는 곳 -> boolean 인수를 제거할 수 있다.
 aShipment.deliveryDate = rushDeliveryDate(anOrder)
 aShipment.deliveryDate = regularDeliveryDate(anOrder)
+
+// -----------------------------------------------------------------------------------
+
+// [해결법] 불리언 리터럴 값을 없애고 명시적인 함수로 바꾸자.
+// 2. 호출하는 곳을 대체했다면 deliveryDate 기존 코드를 제거할 수 있다!
+// function deliveryDate(anOrder, isRush) {
+//   if (isRush) {
+//     return rushDeliveryDate(anOrder)
+//   } else {
+//     return regularDeliveryDate(anOrder)
+//   }
+// }
+
+function rushDeliveryDate(anOrder) {
+  let deliveryTime
+  if (['MA', 'CT'].includes(anOrder.deliveryState)) {
+    deliveryTime = 1
+  } else if (['NY', 'NH'].includes(anOrder.deliveryState)) {
+    deliveryTime = 2
+  } else {
+    deliveryTime = 3
+  }
+  return anOrder.placedOn.plusDay(1 + deliveryTime)
+}
+
+function regularDeliveryDate(anOrder) {
+  let deliveryTime
+  if (['MA', 'CT', 'NY'].includes(anOrder.deliveryState)) {
+    deliveryTime = 2
+  } else if (['NY', 'NH'].includes(anOrder.deliveryState)) {
+    deliveryTime = 3
+  } else {
+    deliveryTime = 4
+  }
+  return anOrder.placedOn.plusDay(1 + deliveryTime)
+}
+
+// 함수를 호출하는 곳 -> boolean 인수를 제거할 수 있다.
+aShipment.deliveryDate = rushDeliveryDate(anOrder)
+aShipment.deliveryDate = regularDeliveryDate(anOrder)
