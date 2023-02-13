798f218b8a7a75cb488f679e4755bee0ad07ee9a
Youn Se Eun <tpdms717@gmail.com>
2023-02-13 21:52:44 +0900

    Refactor code step 1

--- CHAPTER11/11-5/example.js
+++ CHAPTER11/11-5/example.js
@@ -19,3 +19,26 @@ class Order {
     }
   }
 }
+
+// ---------------------------------------------------------------------------------
+
+class Order {
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice
+    return this.discountedPrice(basePrice, this.discountLevel)
+  }
+
+  // 1. '임시 변수를 질의 함수로 바꾸기' 리팩터링 적용.
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1
+  }
+
+  discountedPrice(basePrice, discountLevel) {
+    switch (discountLevel) {
+      case 1:
+        return basePrice * 0.95
+      case 2:
+        return basePrice * 0.9
+    }
+  }
+}
