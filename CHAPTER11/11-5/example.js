3747fafbd2a91e8f73aa10c34c8d711e40318223
Youn Se Eun <tpdms717@gmail.com>
2023-02-13 21:54:40 +0900

    Refactor code step 2~3

--- CHAPTER11/11-5/example.js
+++ CHAPTER11/11-5/example.js
@@ -42,3 +42,29 @@ class Order {
     }
   }
 }
+
+// ---------------------------------------------------------------------------------
+
+class Order {
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice
+    // 3. '함수 선언 바꾸기' 리팩터링으로 매개변수를 없앨 수 있다.
+    return this.discountedPrice(basePrice)
+    // return this.discountedPrice(basePrice, this.discountLevel)
+  }
+
+  // 1. '임시 변수를 질의 함수로 바꾸기' 리팩터링 적용.
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1
+  }
+
+  // 2. discountLevel 매개변수를 참조하는 코드를 모두 함수 호출로 바꾸자.
+  discountedPrice(basePrice) {
+    switch (this.discountLevel) {
+      case 1:
+        return basePrice * 0.95
+      case 2:
+        return basePrice * 0.9
+    }
+  }
+}
