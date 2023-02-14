f7e38f2c16c1db62de1130b10513d151bc4d561c
Youn Se Eun <tpdms717@gmail.com>
2023-02-14 23:48:52 +0900

    Refactor code step 4

--- CHAPTER11/11-9/example.js
+++ CHAPTER11/11-9/example.js
@@ -85,3 +85,46 @@ class Scorer {
 function score(candidate, medicalExam, scoringGuide) {
   return new Scorer(candidate, medicalExam, scoringGuide).execute()
 }
+
+// -----------------------------------------------------------------------------
+
+// 1. 시작은 빈 클래스를 만들고 이 함수를 그 클래스로 옮기는 일부터다.
+class Scorer {
+  // 2. 명령이 받는 인수를 생성자로 옮기자. (= 매개변수를 옮기자.)
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate
+    this._medicalExam = medicalExam
+    this._scoringGuide = scoringGuide
+  }
+
+  // [더 가다듬기] 4. 지역 변수를 필드로 바꾼다.
+  execute() {
+    // let result = 0
+    // let healthLevel = 0
+    // let highMedicalRiskFlag = false
+    this._result = 0
+    this._highLevel = 0
+    this._highMedicalRiskFlag = false
+
+    if (this._medicalExam.isSmoker) {
+      this._highLevel += 10
+      this._highMedicalRiskFlag = true
+    }
+
+    this._certificationGrade = 'regular'
+    if (
+      this._scoringGuide.stateWithLowCertification(this._candidate.originState)
+    ) {
+      this._certificationGrade = 'low'
+      this._result -= 5
+    }
+
+    this._result -= Math.max(this._highLevel - 5, 0)
+    return this._result
+  }
+}
+
+// 3. execute 메서드를 호출하는 곳에서도 인자를 지운다.
+function score(candidate, medicalExam, scoringGuide) {
+  return new Scorer(candidate, medicalExam, scoringGuide).execute()
+}
