7a04df26ada7b4252d81b535004e0ca908b03f91
Youn Se Eun <tpdms717@gmail.com>
2023-02-15 22:49:52 +0900

    Refactor code step 1~2

--- CHAPTER11/11-11/example.js
+++ CHAPTER11/11-11/example.js
@@ -12,3 +12,24 @@ function calcultateAscent() {
     totalAscent += verticalChange > 0 ? verticalChange : 0
   }
 }
+
+// -----------------------------------------------------------------------------------------------
+
+// totalAscent가 갱신된다는 사실이 드러나지 않으므로 갱신 사실을 밖으로 알리자.
+let totalAscent = 0
+let totalTime = 0
+let totalDistance = 0
+// 2. 호출한 곳에서 변수에 대입하게 고친다.
+totalAscent = calculateAscent()
+calculateTime()
+calculateDistance()
+const pace = totalTime / 60 / totalDistance
+
+function calcultateAscent() {
+  for (let i = 1; i < points.length; i++) {
+    const verticalChange = points[i].elevation - points[i - 1].elevation
+    totalAscent += verticalChange > 0 ? verticalChange : 0
+  }
+  // 1. totalAscent 값을 반환하고
+  return totalAscent
+}
