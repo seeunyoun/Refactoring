2c237ced0ca7ed7cb917e4c0a3e1bd293991d06d
Youn Se Eun <tpdms717@gmail.com>
2023-02-21 22:31:35 +0900

    Refactor code step 1~2

--- CHAPTER12/12-3/example1.js
+++ CHAPTER12/12-3/example1.js
@@ -2,19 +2,46 @@ class Party {}
 
 class Employee extends Party {
   constructor(name, id, monthlyCost) {
-    super();
-    this._id = id;
-    this._name = name;
-    this._monthlyCost = monthlyCost;
+    super()
+    this._id = id
+    this._name = name
+    this._monthlyCost = monthlyCost
   }
   // 생략
 }
 
 class Department extends Party {
   constructor(name, staff) {
-    super();
-    this._name = name;
-    this._staff = staff;
+    super()
+    this._name = name
+    this._staff = staff
+  }
+  // 생략
+}
+
+// --------------------------------------------------------
+
+class Party {
+  // 1. name 공통 코드를 슈퍼클래스로 옮긴다.
+  constructor(name) {
+    this._name = name
+  }
+}
+
+class Employee extends Party {
+  constructor(name, id, monthlyCost) {
+    // 2. 슈퍼클래스 생성자에 매개변수로 name을 넘긴다.
+    super(name)
+    this._id = id
+    this._monthlyCost = monthlyCost
+  }
+  // 생략
+}
+
+class Department extends Party {
+  constructor(name, staff) {
+    super(name)
+    this._staff = staff
   }
   // 생략
 }
