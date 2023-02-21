13183f8100e2dcb56cc51ffd22c72d2f56e492d2
Youn Se Eun <tpdms717@gmail.com>
2023-02-21 22:47:07 +0900

    Refactor code step 2

--- CHAPTER12/12-3/example2.js
+++ CHAPTER12/12-3/example2.js
@@ -53,3 +53,44 @@ class Manager extends Employee {
     return this._grade > 4
   }
 }
+
+// ---------------------------------------------------------------
+
+class Employee {
+  constructor(name) {}
+
+  get isPrivileged() {}
+
+  assignCar() {}
+
+  // 2. 추출한 메서드를 슈퍼클래스로 옮긴다.
+  finishConstruction() {
+    if (this.isPrivileged) {
+      return this.assignCar()
+    }
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, grade) {
+    super(name)
+    this._grade = grade
+    // if (this.isPrivileged) {
+    //   return this.assignCar() // 모든 서브클래스가 수행한다.
+    // }
+
+    // 1. 공통 코드를 함수로 출력한다.
+    this.finishConstruction()
+  }
+
+  // 1. 공통 코드를 함수로 출력한다.
+  // finishConstruction() {
+  //   if (this.isPrivileged) {
+  //     return this.assignCar()
+  //   }
+  // }
+
+  get isPrivileged() {
+    return this._grade > 4
+  }
+}
