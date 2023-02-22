4a97353a538d338a56c1377754087cde581229ef
Kaya Yoon <kaya@methinks.io>
2023-02-22 13:17:22 +0900

    Refactor code step1

--- CHAPTER12/12-6/example.js
+++ CHAPTER12/12-6/example.js
@@ -23,3 +23,58 @@ class Female extends Person {
     return "F";
   }
 }
+
+//  ------------------------------------------------
+
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get genderCode() {
+    return "X";
+  }
+}
+
+class Male extends Person {
+  get genderCode() {
+    return "Y";
+  }
+}
+
+class Female extends Person {
+  get genderCode() {
+    return "F";
+  }
+}
+
+// 1. 팩터리 함수를 생성한다.
+function createPerson(aRecord) {
+  let p;
+  switch (aRecord.gender) {
+    case "M": {
+      p = new Male(aRecord.name);
+      break;
+    }
+    case "F": {
+      p = new Female(aRecord.name);
+      break;
+    }
+    default: {
+      p = new Person(aRecord.name);
+    }
+  }
+  return p;
+}
+
+function loadFromInput(data) {
+  const result = [];
+  data.forEach((aRecord) => {
+    result.push(createPerson(aRecord));
+  });
+  return result;
+}
