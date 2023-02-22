5d5aff4bc5053914613d860082d4e630b77b2523
Kaya Yoon <kaya@methinks.io>
2023-02-22 12:49:40 +0900

    Refactor code step 1~2

--- CHAPTER12/12-5/example2.js
+++ CHAPTER12/12-5/example2.js
@@ -29,3 +29,56 @@ class Employee {
     return `${this.name} (${this.capitalizedType})`;
   }
 }
+
+// -----------------------------------------------------------------------------------------------------
+// [간접 상속할 때]
+
+class Employee {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+
+  // 2. getter 추가
+  get typeString() {
+    return this._type.toString();
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  set type(arg) {
+    this._type = arg;
+  }
+
+  // 2. getter로 변경!
+  get capitalizedType() {
+    return (
+      this.typeString.charAt(0).toUpperCase() +
+      this.typeString.substr(1).toLowerCase()
+    );
+  }
+
+  toString() {
+    return `${this.name} (${this.capitalizedType})`;
+  }
+}
+
+// 1. 타입 코드를 객체로 바꾸기
+class EmployeeType {
+  constructor(aString) {
+    this._value = aString;
+  }
+
+  toString() {
+    return this._value;
+  }
+}
