02d28dc1ec01e1517414792a2a0bf1fd91b25b4f
Kaya Yoon <kaya@methinks.io>
2023-02-22 13:01:07 +0900

    Refactor code step 5

--- CHAPTER12/12-5/example2.js
+++ CHAPTER12/12-5/example2.js
@@ -170,3 +170,84 @@ class Salesperson extends EmployeeType {
     return "salesperson";
   }
 }
+
+// -----------------------------------------------------------------------------------------------------
+// [간접 상속할 때]
+
+class Employee {
+  constructor(name, type) {
+    this.validateType(type);
+    this._name = name;
+    this._type = type;
+  }
+
+  validateType(arg) {
+    if (!["engineer", "manager", "salesperson"].includes(arg)) {
+      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
+
+  // 2. getter 추가
+  get typeString() {
+    return this._type.toString();
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  // 4. setter도 수정한다.
+  set type(arg) {
+    this._type = Employee.createEmployeeType(arg);
+  }
+
+  // 3. 직원 유형을 예시1과 같이 리팩터링한다.
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer": {
+        return new Engineer();
+      }
+      case "manager": {
+        return new Manager();
+      }
+      case "salesperson": {
+        return new Salesperson();
+      }
+      default: {
+        throw new Error(`${aString}라는 직원 유형은 없습니다.`);
+      }
+    }
+  }
+
+  toString() {
+    return `${this.name} (${this.type.capitalizedType})`;
+  }
+}
+
+// 1. 타입 코드를 객체로 바꾸기
+class EmployeeType {
+  // 5. capitalizedName 코드를 이 클래스로 옮긴다.
+  get capitalizedType() {
+    return (
+      this.toString().charAt(0).toUpperCase() +
+      this.toString().substr(1).toLowerCase()
+    );
+  }
+}
+
+// 3. 직원 유형을 예시1과 같이 리팩터링한다.
+class Engineer extends EmployeeType {
+  toString() {
+    return "engineer";
+  }
+}
+class Manager extends EmployeeType {
+  toString() {
+    return "manager";
+  }
+}
+class Salesperson extends EmployeeType {
+  toString() {
+    return "salesperson";
+  }
+}
