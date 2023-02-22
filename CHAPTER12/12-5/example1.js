6d1d8248c6addd5ef0cca90bfb27f53d7010a40e
Kaya Yoon <kaya@methinks.io>
2023-02-22 12:41:26 +0900

    Refactor code step 9

--- CHAPTER12/12-5/example1.js
+++ CHAPTER12/12-5/example1.js
@@ -183,3 +183,51 @@ function createEmployee(name, type) {
   }
   //   return new Employee(name, type);
 }
+
+// -----------------------------------------------------------------------------
+// [직접 상속할 때]
+
+class Employee {
+  // 9. 마지막으로 생성자에 type을 넘길 필요가 없으므로 type 인수를 제거한다.
+  constructor(name) {
+    this._name = name;
+  }
+
+  toString() {
+    return `${this.name} (${this.type})`;
+  }
+}
+
+class Engineer extends Employee {
+  get type() {
+    return "engineer";
+  }
+}
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+class Salesperson extends Employee {
+  get type() {
+    return "salesperson";
+  }
+}
+
+function createEmployee(name, type) {
+  switch (type) {
+    // 9. type 인수를 제거한다.
+    case "engineer": {
+      return new Engineer(name);
+    }
+    case "manager": {
+      return new Manager(name);
+    }
+    case "salesperson": {
+      return new Salesperson(name);
+    }
+    default: {
+      throw new Error(`${type}이라는 직원 유형은 없습니다.`);
+    }
+  }
+}
