ff5edf94c5b76b9e4deedb019b98b755c92ecb3e
Kaya Yoon <kaya@methinks.io>
2023-02-22 17:17:10 +0900

    Refactor code step 1~5

--- CHAPTER12/12-8/example.js
+++ CHAPTER12/12-8/example.js
@@ -50,3 +50,65 @@ class Department {
     return this.totalMonthlyCost * 12;
   }
 }
+
+// ---------------------------------------------------------------------------
+
+// 1. 빈 슈퍼클래스를 만들고, 두 클래스가 이를 확장하도록 한다.
+class Party {
+  // 2. 데이터부터 바꿔나가자. 이름 속성을 먼저 위로 올린다.(필드 올리기)
+  constructor(name) {
+    this._name = name;
+  }
+
+  // 3. 필드 올리기를 했으면 메서드를 올린다.
+  get name() {
+    return this._name;
+  }
+}
+
+class Example extends Party {
+  constructor(name, id, monthlyCost) {
+    super(name);
+    this._id = id;
+    this._monthlyCost = monthlyCost;
+  }
+
+  get monthlyCost() {
+    return this._monthlyCost;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  constructor(name, staff) {
+    super(name);
+    this._staff = staff;
+  }
+
+  get staff() {
+    return this._staff.slice();
+  }
+
+  // 4. (함수 선언 바꾸기 리팩터링)으로 이름을 통일한다.
+  get monthlyCost() {
+    return this.staff
+      .map((e) => e.monthlyCost)
+      .reduce((sum, cost) => sum + cost);
+  }
+
+  get headCount() {
+    return this.staff.length;
+  }
+
+  // 5. 4번과 같은 방식으로 (함수 선언 바꾸기 리팩터링)으로 이름을 통일한다.
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
